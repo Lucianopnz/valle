fc8ac32f96b3a3c24d8ff688fb5b928c7d844009
Lucianopnz <106544723+Lucianopnz@users.noreply.github.com>
2022-12-17 15:18:15 -0300

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ router.get('/', function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
 })
 
+router.get('/sobre', function(req,res){
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+})
+
 app.use('/', router);
 app.listen(process.env.port || 3000, ()=>{
     console.log("Servidor rodando com sucesso...")
